cdf5dead58315d41585e3b80687f6dc9958051cf
Stephan Karl <steph@Stephans-MBP.home>
2024-06-02 13:09:55 -0700

    initial commit with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Typography } from '@mui/material';
+
+import TimeToBaby from './components/TimeToBaby';
+import TimeForCountry from './components/TimeForCountry';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container justifyContent='center' spacing={3} my={3}>
+      <Grid item xs={12} my={3}>
+        <Typography textAlign='center' variant='h2'>Time for babies and country</Typography>
+      </Grid>
+      <TimeToBaby />
+      <TimeForCountry />
+    </Grid>
   );
 }
 
---
+++ src/components/TimeForCountry.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Grid, Typography } from '@mui/material'
+import { MusicNote } from '@mui/icons-material'
+
+import moment from 'moment'
+
+export default function TimeForCountry() {
+
+    const moveDate = moment('2024-10-01')
+    const today = moment()
+    const daysToCountry = moveDate.diff(today, 'days')
+
+  return (
+    <Grid container item xs={8} justifyContent='center' >
+        <Grid item xs={12} mb={3}>
+            <Typography textAlign='center' variant='h5'>Time For Country</Typography>
+        </Grid>
+        <Grid item xs={12} display='flex' justifyContent='center'  >
+            <MusicNote sx={{ alignSelf: 'center', mx: 1, color:'#d60d0d', fontSize: 70 }} />
+            <Typography textAlign='center' variant='h2'>{daysToCountry} days</Typography>
+            <MusicNote sx={{ alignSelf: 'center', mx: 1, color:'#d60d0d', fontSize: 70 }} />
+        </Grid>
+    </Grid>
+  )
+}
